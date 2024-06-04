bf52ed65762cdf3da4aad41a4d54cc82edcd112a
Mariusz <blimsx@gmail.com>
2024-06-04 18:58:49 +0200

    add while loop in task02

--- 02/app.js
+++ 02/app.js
@@ -4,3 +4,11 @@ let result = 0;
 for (i = 1; i <= x; i++) {
     result = result + i;
 }
+
+result = 0;
+let counter = 1;
+
+while (counter < x+1) {
+    result = result + counter;
+    counter++; 
+}
