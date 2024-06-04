ff4b59f10d9b0027af973e1a518a2f3019517286
zero_jeden <zeroitjeden@gmail.com>
2024-06-04 22:18:37 +0200

    solution 1 & 2

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,12 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+let allNums;
+
+if (randomNumber >= 5) {
+	for (i = 5; i < randomNumber; i++) {
+		console.log('wcześniejsza liczba to: ' + i);
+	}
+} else {
+	console.log('ta liczba jest zbyt mała, aby pętla zadziałała!');
+}
+
+console.log('wylosowana liczba to: ' + randomNumber);
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+for(i = 1; i <= x; i++){
+    console.log(result = result + i);
+}
+
+console.log('______________pętla while zacznie dodawać do 15')
+
+let j = 1;
+while(j <= x) {
+    console.log(result = result + j);
+    j++;
+}
