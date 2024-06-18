3d0b883cc54555709923ee5a00c21460c90cff7d
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-06-18 21:49:23 +0200

    Add task 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x !== randomNumber) {
+  randomNumber = Math.round(Math.random() * 20);
+  iteration = iteration + 1;
+//   console.log(iteration);
+//   console.log(randomNumber);
+}
+
+console.log("Wylosowano za " + iteration + " razem");
