7085aaddd917500b7d65ff92a40885afdb200c5f
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-20 17:17:45 +0200

    added ; to end lane

--- 01/app.js
+++ 01/app.js
@@ -5,7 +5,7 @@ console.log(randomNumber);
 if (randomNumber > 5) {
     for (let i = 5; i <= randomNumber; ++i) {
         console.log(i);
-    }
+    };
 } else {
     console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli")
-}
+};
