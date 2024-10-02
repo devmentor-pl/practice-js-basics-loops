87296d2731dbf2dbbf05d49a82f38d34c167dc31
Marek Bełz <belzmarek@gmail.com>
2024-10-02 18:04:54 +0000

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,20 @@
 const x = 5;
-let result = 0;
+let result1 = 0;
+let result2 = 0;
+let j = 1;
+
+
+for (let i = 1; i <= x; i++) {
+    result1 += i; 
+}
+console.log(result1);
+
+
+
+
+
+while(j<=x){
+    result2 +=j;
+    j++;
+}
+console.log(result2);
