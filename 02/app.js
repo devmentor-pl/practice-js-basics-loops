e6975941947692ddf733f3c844fbaa8231a3aff7
marrcelp <marcel.piaszczyk@gmail.com>
2024-11-24 16:17:28 +0100

    resolved ex 2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+    result += i; 
+}
+console.log(`Suma dla podanych liczb to ${result}`);
+
+let j = 1;
+let sum = 0;
+
+while (j <= x){
+    sum += j;
+    j++;
+}
+console.log(`Suma dla podanych liczb za pomoca while to ${sum}`);
