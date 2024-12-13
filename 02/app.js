74d73dc5b38f3aa0eba42f3b8614adfa3bf88f35
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-13 22:09:12 +0100

    task 2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,10 @@
 const x = 5;
 let result = 0;
+let i = 1;
+
+while (i <= x) {
+  result += i;
+  console.log(`Suma kolejnych ${i} liczb wynosi ${result}`);
+  i++;
+}
+console.log(`Suma kolejnych liczb od 1 do x wynosi ${result}`);
