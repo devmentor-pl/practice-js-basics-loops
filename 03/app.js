b90a5e8d88d19085f4b07566a8b4ce6962c9b017
Daria Seńko <twoj@email>
2025-03-10 17:57:06 +0100

    zad 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,9 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+  randomNumber = Math.round(Math.random() * 21);
+  iteration++;
+}
+console.log('Liczba prób: ' + iteration);
