8ca6cb042e1669847ae7f4c321329fd83203d77d
Zaneta Sochon <twj email>
2025-04-03 20:06:44 +0200

    feat: Create while loop with randomNumber

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x !== randomNumber) {
+  const number = Math.round(Math.random() * x);
+  randomNumber = number;
+  iteration++;
+}
+
+console.log(iteration);
