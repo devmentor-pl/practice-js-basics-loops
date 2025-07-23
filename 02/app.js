8c93435ac6d26e824605784f5af90e86d29fc333
Natalia Kur <natalia.kur@yahoo.com>
2025-07-23 17:20:34 +0200

    Complete task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
-let result = 0;
+
+let resultFor = 0;
+for (let i = 1; i <= x; i++) {
+  resultFor = resultFor + i;
+  console.log(resultFor);
+}
+
+let resultWhile = 0;
+let j = 1;
+while (j <= x) {
+  resultWhile = resultWhile + j;
+  j++;
+  console.log(resultWhile);
+}
